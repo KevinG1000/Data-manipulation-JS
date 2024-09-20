f4514409dff238ba825740d20fa4866ab94b332c
KevinG1000 <Gonzalezkevin443@gmail.com>
2024-09-20 12:06:04 -0400

    data-manipulation-lab

---
+++ main.js
@@ -0,0 +1,38 @@
+// LAB 308.1.1 
+
+
+function roadTrip (speed, mpg)
+    const totalDistance = 1500
+    const fuelCostPG = 3
+    const fuelBudget = 175
+    
+    let lowMile = 30 mpg
+    let medMile =  28 mpg
+    let highMile = 23 mpg
+
+    
+
+    totalFuelCost = total
+
+
+// 1. Fuel needed for the entire trip?
+
+// @ 55 miles / 30 mi/g --> 50 gallons of fuel will be needed
+// @ 60 miles / 28 mi/g --> 53.6 gallons of fuel will be needed
+// @ 75 miles / 23 mi/g --> 65 gallons of fuel will be needed
+
+gallonsNeeded = totalDistance / mpg;
+
+
+// 2. Will your budget be enough to vover the fuel expense?
+
+const totalFuelCost = gallonsNeeded + fuelCostPG;
+
+const isBudgetEnough = 
+
+
+
+
+
+
+const gallonsnNeeded = totalDistance / mpg
